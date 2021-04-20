b0778e7ae2a5d80a007c567f74441ad4e6400c27
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-04-21 00:38:22 +0200

    Update Mon_code.py

--- package/drawingFourier/Mon_code.py
+++ package/drawingFourier/Mon_code.py
@@ -1,3 +1,74 @@
+import cv2 # for finding contours to extract points of figure
+import matplotlib.pyplot as plt # for plotting and creating figures
+import numpy as np # for easy and fast number calculation
+from math import tau # tau is constant number = 2*PI
+from scipy.integrate import quad_vec, quad # for calculating definite integral 
+from tqdm import tqdm # for progress bar
+import matplotlib.animation as animation # for compiling animation and exporting video 
+#%%
+
+### Partie Kenjy
+
+def create_close_loop(image_name):
+    # read image to array, then get image border with contour
+    # reading the image and convert to greyscale mode
+    # ensure that you use image with black image with white background
+    img = cv2.imread(image_name)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Get Contour Path and create lookup-table
+    # find the contours in the image
+    ret, thresh = cv2.threshold(img_gray, 127, 255, 0) # making pure black and white image
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) # finding available contours i.e closed loop objects
+    contours = np.array(contours[1]) # contour at index 1 is the one we are looking for
+
+    # split the co-ordinate points of the contour
+    # we reshape this to make it 1D array
+    x_list, y_list = contours[:, :, 0].reshape(-1,), -contours[:, :, 1].reshape(-1,)
+    # time data from 0 to 2*PI as x,y is the function of time.
+    t_list = np.linspace(0, tau, len(x_list)) # now we can relate f(t) -> x,y
+
+    # center the contour to origin
+    x_list = x_list - np.mean(x_list)
+    y_list = y_list - np.mean(y_list)
+
+    # visualize the contour
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(x_list, y_list)
+
+    # later we will need these data to fix the size of figure
+    xlim_data = plt.xlim() 
+    ylim_data = plt.ylim()
+
+    return t_list, x_list, y_list
+
+#%%
+t_list, x_list, y_list = create_close_loop("star.jpeg")
+print(t_list, x_list, y_list)
+#%%
+
+##### Partie Chloe 
+
+# function to generate x+iy at given time t
+def f(t, t_list, x_list, y_list):
+    return np.interp(t, t_list, x_list + 1j*y_list)
+
+#%%
+def coef_list(t_list, x_list, y_list, order=10):
+    coef_list = []
+    for n in range(-order, order+1):
+        real_coef = quad(lambda t: np.real(f(t, t_list, x_list, y_list) * np.exp(-n*1j*t)), 0, tau, limit=100, full_output=1)[0]/tau
+        imag_coef = quad(lambda t: np.imag(f(t, t_list, x_list, y_list) * np.exp(-n*1j*t)), 0, tau, limit=100, full_output=1)[0]/tau
+        coef_list.append([real_coef, imag_coef])
+    return np.array(coef_list)
+
+#%%
+
+order = 10
+cf = coef_list(t_list, x_list, y_list, order)
+print(cf)
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
@@ -99,16 +170,16 @@ class FS():
                                    color = 'red')
             axs[1].add_artist(con)
 
-            plt.pause(1e-14)
+            plt.pause(1e-10)
 
 if __name__ == '__main__':
     '''
-    Example :
-    with 3 circles and 2 repetitions
-
+       Visualize star
     '''
-    cir = 3 
-    cycles = 2 
-    fcoef = [2 + 1j, 1+1j, 1-1j] 
+    cir = len(cf)
+    cycles = 1
+    fcoef = np.empty(len(cf), dtype=complex)
+    for i in range(len(cf)):
+        fcoef[i] = np.complex(cf[i][0],cf[i][1])
     fs = FS(cir,cycles,fcoef)
     fs.PlotFS()
