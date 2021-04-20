cc64e628ad3fc8125e8cfb281b04ffc688bef8db
Kenjy <kenjy.koan@gmail.com>
2021-04-20 21:23:33 +0200

    add packages needed

--- Code Kenjy final/image_manip.py
+++ Code Kenjy final/image_manip.py
@@ -1,7 +1,11 @@
 #%%
 # import packages
-import numpy as np
+import numpy as np 
+import matplotlib.pyplot as plt
 from PIL import Image
+from numpy import interp
+from math import tau
+from scipy.integrate import quad
 
 #%%
 
