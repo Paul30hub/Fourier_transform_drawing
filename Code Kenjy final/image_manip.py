c1b07ce7128a813c0ec8b3a05e1abb42a9468dfd
Kenjy <kenjy.koan@gmail.com>
2021-04-20 21:26:10 +0200

    Revert "test function"
    This reverts commit 712572eca76dac635e85cd60f977ac3679a81b90.

--- Code Kenjy final/image_manip.py
+++ Code Kenjy final/image_manip.py
@@ -44,7 +44,3 @@ class ImageReader:
         ax[1].plot(self.x_table , self.y_table, 'k-')
         
         return self.time_table, self.x_table, self.y_table
-
-#test function 
-#image = ImageReader("star.jpeg")
-#image.get_tour()
