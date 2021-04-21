2598410473c5809261ae6a3c1feeea682bf45edb
Kenjy <kenjy.koan@gmail.com>
2021-04-21 10:23:23 +0200

    set up sphynx documentation

--- Code Kenjy final/DFT.py
+++ Code Kenjy final/DFT.py
@@ -45,7 +45,36 @@ def create_close_loop(image_name):
     return t_list, x_list, y_list
 
 #%%
-t_list, x_list, y_list = create_close_loop("star.jpeg")
+def create_close_loop(image_name, level=[200]):
+    # Prepare Plot
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    ax[0].set_aspect('equal', 'datalim')
+    ax[1].set_aspect('equal', 'datalim')
+    ax[0].set_title('Before Centered')
+    ax[1].set_title('After Centered')
+
+    # read image to array, then get image border with contour
+    im = array(Image.open(image_name).convert('L'))
+    contour_plot = ax[0].contour(im, levels=level, colors='black', origin='image')
+
+    # Get Contour Path and create lookup-table
+    contour_path = contour_plot.collections[0].get_paths()[0]
+    x_table, y_table = contour_path.vertices[:, 0], contour_path.vertices[:, 1]
+    time_table = np.linspace(0, tau, len(x_table))
+
+    # Simple method to center the image
+    x_table = x_table - min(x_table)
+    y_table = y_table - min(y_table)
+    x_table = x_table - max(x_table) / 2
+    y_table = y_table - max(y_table) / 2
+
+    # Visualization
+    ax[1].plot(x_table, y_table, 'k-')
+
+    return time_table, x_table, y_table
+
+#%%
+t_list, x_list, y_list = create_close_loop("velo.jpeg")
 print(t_list, x_list, y_list)
 #%%
 
@@ -65,10 +94,10 @@ def coef_list(t_list, x_list, y_list, order=10):
     return np.array(coef_list)
 
 #%%
-
 order = 50
 cf = coef_list(t_list, x_list, y_list, order)
 print(cf)
+
 #%%
 def DFT(t, coef_list, order=10):
     kernel = np.array([np.exp(-n*1j*t) for n in range(-order, order+1)])
---
+++ doc/source/index.rst
@@ -0,0 +1,30 @@
+.. Fourier-Transform-Drawing documentation master file, created by
+   sphinx-quickstart on Wed Apr 21 10:19:08 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Fourier-Transform-Drawing's documentation!
+=====================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+.. image:: /_static/cat.png
+   :height: 100
+   :width: 200
+   :scale: 50
+   :align: center
+   :alt: ordinateur
+
+   Caption text rendered below the image...
