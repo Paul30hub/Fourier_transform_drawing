58987757dc93305071583d2bfbf797444a1f04db
CHLOE CORDOVAL <c.cordoval19@gmail.com>
2021-04-22 23:51:05 +0200

    Update CoeffFourier.py

--- Package/Package4/CoeffFourier/CoeffFourier.py
+++ Package/Package4/CoeffFourier/CoeffFourier.py
@@ -12,6 +12,7 @@ class Fourier :
         self.time_table = time_table
         self.x_table = x_table
         self.y_table = y_table
+
     #@staticmethod
 
     def coef_list(self,time_table, x_table, y_table, order) :
@@ -24,8 +25,8 @@ class Fourier :
             
         for n in range(-order, order+1) :
 
-            real_coef = quad(lambda t : np.real(func (t,self.time_table, self.x_table, 1j*self.y_table) * np.exp(-n * 1j * t)), 0, tau, limit=100, full_output=1)[0]/tau
-            imag_coef = quad(lambda t : np.imag(func (t,self.time_table, self.x_table, 1j*self.y_table) * np.exp(-n * 1j *t)), 0, tau, limit=100, full_output=1)[0]/tau
+            real_coef = quad(lambda t : np.real(func(t,self.time_table, self.x_table, 1j*self.y_table) * np.exp(-n * 1j * t)), 0, tau, limit=100, full_output=1)[0]/tau
+            imag_coef = quad(lambda t : np.imag(func(t,self.time_table, self.x_table, 1j*self.y_table) * np.exp(-n * 1j *t)), 0, tau, limit=100, full_output=1)[0]/tau
 
             self.real_coef = real_coef
             self.imag_coef = imag_coef
@@ -35,9 +36,13 @@ class Fourier :
 
         return np.array(coef_list)
 
-    def DFT(self, t, coef_list, order=10):
 
+    def DFT(self, t, coef_list, order=10):
+        
         kernel = np.array([np.exp(-n*1j*t) for n in range(-order, order+1)])
-        series = np.sum( (coef_list[:,0]+1j*coef_list[:,1]) * kernel[:])
+        series = np.sum((coef_list[:,0]+1j*coef_list[:,1]) * kernel[:])
+
+        self.kernel = kernel
+        self.series = series
 
         return np.real(series), np.imag(series)
