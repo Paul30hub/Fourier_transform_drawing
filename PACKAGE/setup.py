dee4a8808c522c27cb0046803157516da6ad3ef9
Pierre Guillaumont <pierre.guillaumont@gmail.com>
2021-04-24 00:27:18 +0200

    long description pypi

--- PACKAGE/setup.py
+++ PACKAGE/setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 from draw_circle_fourier import __version__ as current_version
 
 setup(
-  name='draw_circle_fourier',
-  version=current_version,
-  description='Drawing spinning circle with Fourier',
-  url='https://github.com/Paul30hub/Fourier-transform-drawing',
-  author='Burgat Paul, Cordoval Chloë, Guillaumont Pierre, Koan Kenjy',
-  author_email='paul.burgat@etu.umontpellier.fr, chloe.cordoval@etu.umontpellier.fr, pierre.guillaumont@etu.umontpellier.fr, kenjy.koan@etu.umontpellier.fr',
-  license='MIT',
-  packages=['draw_circle_fourier','draw_circle_fourier.CoeffFourier', 'draw_circle_fourier.ImageAnimation', 'draw_circle_fourier.ImageReader'],
-  zip_safe=False
+  name = 'draw_circle_fourier',
+  version = current_version,
+  description = 'Drawing spinning circle with Fourier',
+  url = 'https://github.com/Paul30hub/Fourier-transform-drawing',
+  author = 'Burgat Paul, Cordoval Chloë, Guillaumont Pierre, Koan Kenjy',
+  author_email = 'paul.burgat@etu.umontpellier.fr, chloe.cordoval@etu.umontpellier.fr, pierre.guillaumont@etu.umontpellier.fr, kenjy.koan@etu.umontpellier.fr',
+  license = 'MIT',
+  packages = ['draw_circle_fourier','draw_circle_fourier.CoeffFourier', 'draw_circle_fourier.ImageAnimation', 'draw_circle_fourier.ImageReader'],
+  zip_safe = False,
+  long_description = 'This package is draw an image using Fourier coefficients. The output is a .gif'
 )
