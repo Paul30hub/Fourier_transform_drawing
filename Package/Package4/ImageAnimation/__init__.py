7e2b3ca44c7d349018cb84df34f8f52705703b33
kkenjy <78488067+Kenjy-K@users.noreply.github.com>
2021-04-23 16:06:26 +0200

    Delete Package directory

--- Package/Package4/ImageAnimation/__init__.py
+++
@@ -1,6 +0,0 @@
-
-if __name__ == "main":
-    PlotFS()
-
-
-
--- Package/Package4/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ImageReader.Image import ImageReader
-from .CoeffFourier.CoeffFourier import Fourier
-from .ImageAnimation.AnimationImage import DrawAnimation
-
