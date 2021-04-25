64ce6c19f2fca099a8fc43c1f4b179c28008e918
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-04-25 20:41:46 +0200

    Update tests_DrawAnimation.py

--- PACKAGE/tests/tests_DrawAnimation.py
+++ PACKAGE/tests/tests_DrawAnimation.py
@@ -4,7 +4,7 @@ import unittest
 class TestClass_DrawAnimation(unittest.TestCase):
 
     def testUpdate_c(self):
-        r = cd.update_c(coef, 25/ len(np.linspace(0,tau,300)) * tau) #Exemple avec i=25, space = np.linspace(0,tau,300)
+        r = cd.update_c(fouriercoef, 25/ len(np.linspace(0,tau,300)) * tau) #Exemple avec i=25, space = np.linspace(0,tau,300)
         self.assertIs(type(r), ndarray)
 
     def testSort_velocity(self):
