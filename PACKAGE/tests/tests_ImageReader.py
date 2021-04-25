c9d709fcc366d923ef0b2e563a967d266225c920
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-04-25 16:21:59 +0200

    Update and rename test_image.py to tests_ImageReader.py

---
+++ PACKAGE/tests/tests_ImageReader.py
@@ -0,0 +1,13 @@
+    import UnitTest.ImageReader as ir
+import unittest
+
+class TestClass_ImageReader(unittest.TestCase):
+
+    def testGet_tour(self):
+        r = ir.get_tour(np.linspace(0,tau,len(contour_path.vertices[:,0])), contour_path.vertices[:,0], contour_path.vertices[:,1])
+        self.assertIs(type(r[0]), tuple)
+        self.assertIs(type(r[1]), int)
+        self.assertIs(type(r[2]), int)
+
+if __name__ = "__main__":
+    unittest.main()        
