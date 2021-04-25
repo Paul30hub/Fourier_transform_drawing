7162551648e39bfcec8303e7182ae5faa95770e4
GuillaumontP <77720503+GuillaumontP@users.noreply.github.com>
2021-04-25 15:47:39 +0200

    Update CoeffFourier.py

--- PACKAGE/draw_circle_fourier/CoeffFourier/CoeffFourier.py
+++ PACKAGE/draw_circle_fourier/CoeffFourier/CoeffFourier.py
@@ -9,7 +9,7 @@ def f(t, time_table, x_table, y_table):
             Convert the X and Y coords to complex number over time
             """
             X = np.interp(t, time_table, x_table) 
-            Y = 1j*np.interp(t, time_table, y_table)
+            Y = 1j * np.interp(t, time_table, y_table)
             return X + Y
 
 class Fourier :
@@ -37,8 +37,8 @@ class Fourier :
             
         for n in range(-order, order+1):
         #integrate across f .
-            real_coef = quad(lambda t: np.real(f(t, time_table, x_table, y_table) * np.exp(-n*1j*t)), 0, tau, limit=100, full_output=1)[0]/tau
-            imag_coef = quad(lambda t: np.imag(f(t, time_table, x_table, y_table) * np.exp(-n*1j*t)), 0, tau, limit=100, full_output=1)[0]/tau
+            real_coef = quad(lambda t: np.real(f(t, time_table, x_table, y_table) * np.exp(-n * 1j * t)), 0, tau, limit=100, full_output=1)[0] / tau
+            imag_coef = quad(lambda t: np.imag(f(t, time_table, x_table, y_table) * np.exp(-n * 1j * t)), 0, tau, limit=100, full_output=1)[0] / tau
             coef_list.append([real_coef, imag_coef])
 
         return np.array(coef_list)
@@ -46,7 +46,7 @@ class Fourier :
 
     def DFT(self, t, coef_list, order):
     #Compute the discrete fourier series with a given order
-        kernel = np.array([np.exp(-n*1j*t) for n in range(-order, order+1)])
-        series = np.sum( (coef_list[:,0]+1j*coef_list[:,1]) * kernel[:])
+        kernel = np.array([np.exp(-n * 1j * t) for n in range(-order, order+1)])
+        series = np.sum( (coef_list[:,0] + 1j * coef_list[:,1]) * kernel[:])
 
         return np.real(series), np.imag(series)
