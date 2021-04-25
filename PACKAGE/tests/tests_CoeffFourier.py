a58aab688a0cb3865cbf6ac4811559181db4cedf
Paul30hub <78489315+Paul30hub@users.noreply.github.com>
2021-04-25 17:00:32 +0200

    Update tests_CoeffFourier.py

--- PACKAGE/tests/tests_CoeffFourier.py
+++ PACKAGE/tests/tests_CoeffFourier.py
@@ -1,1 +1,16 @@
+import UnitTest.Fourier as f
+import unittest
 
+class TestClass_Fourier(unittest.TestCase):
+    
+    def testCoef_list(self):
+        c = f.coef_list(np.linspace(0,tau,len(contour_path.vertices[:,0])), contour_path.vertices[:,0], contour_path.vertices[:,1], 50) # Exemple avec order = 50
+        self.assertIs(type(c), ndarray)
+    
+    def testdft(self):
+        d = f.DFT(2, [-0.466329862, 0.546912553], 50) #Exemple avec t=2, coef_list=([-0.466329862, 0.546912553] et order=50
+        self.assertIs(type(d), list)
+
+        
+if __name__ == "__main__":
+    unittest.main()        
